4e4d672c7a6b4fcad48e631d251dbfb02178e646
Ann-Katrine <annx3683@edu.sde.dk>
2018-01-05 10:39:39 +0100

    deltager virker med alt

--- spil.cs
+++ spil.cs
@@ -10,7 +10,9 @@ namespace Ludo
     class Spil
     {
         private int deltager;
-        
+        private Spillere[] spillere;
+
+
         public Spil()
         {
             Console.WriteLine("Velkommen til Ludo");
@@ -21,26 +23,27 @@ namespace Ludo
 
         private void setdeltager()
         {
-            
-
             do
             {
                 Console.WriteLine("Hvor mange deltage 2-4?: ");
-                deltager = Convert.ToInt32(Console.ReadLine());
-
-                if (deltager >= 2 && deltager <= 4)
+                try
                 {
-
+                    deltager = Convert.ToInt32(Console.ReadLine());
                 }
-                else 
+                catch (Exception)
                 {
-                    Console.WriteLine("Det er ikke den rigtig værdi, prøv igen.");
-                    Console.WriteLine("");
+                    Console.WriteLine("Det er ikke den rigtige værdi, prøv igen.");
                 }
-                
             } while (deltager < 2 || deltager >4);
         }
 
+        private void lavspiller()
+        {
+            Console.WriteLine("Skriv dit spillernavn?: ");
+            this.spillere = new Spillere[this.deltager];
+
+
+        }
    
     }
 }
